7426d4258ee743ea597bf5963005a6b130cb232b
Stuart Haigh <stuarth@greenshootlabs.com>
2019-07-08 15:13:19 +0100

    updates the update script to work in different environments

--- resources/scripts/update-web-chat.sh
+++ resources/scripts/update-web-chat.sh
@@ -1,27 +1,87 @@
 #!/usr/bin/env bash
 
-environment='dev'
-if [[ "${1}" = "prod" || "${1}" = "production" ]]; then
-  environment='prod'
+usage="$(basename "$0") [-h n] [-p n] [-l n] [-i n] [-f n] -- Sets up the OpenDialog webchat dependencies after an update
+
+where:
+    -h show this help text
+    -p is the environment prod (defaults: false)
+    -l whether to run for lando environment (default: false)
+    -i whether to re-install node dependencies or not (defaults: false)
+    -f whether to force refresh meaning deleting local dependencies before installing (defaults: false)"
+
+environment="dev"
+lando=false
+install=false
+refresh=false
+while getopts ':hpliff:' option; do
+  case "$option" in
+    h) echo "$usage"
+       exit
+       ;;
+    p) environment="prod"
+       ;;
+    l) lando=true
+       ;;
+    f) refresh=true
+       ;;
+    i) install=true
+       ;;
+    :) printf "missing argument for -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+   \?) printf "illegal option: -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+  esac
+done
+
+if ${lando}
+then
+   pre="lando"
+else
+   pre=""
 fi
 
 echo "Running setup for ${environment} environment"
 
+read -r -p "Do you want to continue? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+then
+    echo "Continuing..."
+else
+   echo "Ending execution"
+   exit
+fi
+
+
 cd vendor/opendialogai/webchat
 
-rm -rf node_modules/vue-beautiful-chat
+if (${refresh})
+then
+    echo "Refreshing local dependencies"
+    rm -rf node_modules/vue-beautiful-chat
+fi
 
-npm install
 
-npm run ${environment}
+if (${install})
+then
+    echo "Installing node dependencies"
+    eval ${pre} npm install
+fi
+
+echo "Running ${pre} npm run"
+eval ${pre} npm run ${environment}
 
 cd ../../../
 
+echo "Deleting local public files"
 rm -r public/vendor/webchat
 
-php artisan vendor:publish --tag=public --force
+eval ${pre} php artisan vendor:publish --tag=public --force
 
-php artisan vendor:publish --tag=vue-components --force
+eval ${pre} php artisan vendor:publish --tag=vue-components --force
 
 echo "Updating JS and CSS version string"
 
