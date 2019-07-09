58cc490fce543e649fab22659da2c3d7d16ad60a
Stuart Haigh <stuarth@greenshootlabs.com>
2019-07-09 17:40:20 +0100

    allows skipping of questions

--- resources/scripts/update-web-chat.sh
+++ resources/scripts/update-web-chat.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
-usage="$(basename "$0") [-h n] [-p n] [-l n] [-i n] [-f n] -- Sets up the OpenDialog webchat dependencies after an update
+usage="$(basename "$0") [-h n] [-p n] [-l n] [-i n] [-f n] [-y n]-- Sets up the OpenDialog webchat dependencies after an update
 
 where:
     -h show this help text
     -p is the environment prod (defaults: false)
     -l whether to run for lando environment (default: false)
     -i whether to re-install node dependencies or not (defaults: false)
-    -f whether to force refresh meaning deleting local dependencies before installing (defaults: false)"
+    -f whether to force refresh meaning deleting local dependencies before installing (defaults: false)
+    -y whether to ask questions or not. If set, install will assume yes to all"
 
 environment="dev"
 lando=false
 install=false
 refresh=false
-while getopts ':hpliff:' option; do
+ask=true
+while getopts ':hpliyff:' option; do
   case "$option" in
     h) echo "$usage"
        exit
@@ -25,6 +27,8 @@ while getopts ':hpliff:' option; do
     f) refresh=true
        ;;
     i) install=true
+       ;;
+    y) ask=false
        ;;
     :) printf "missing argument for -%s\n" "$OPTARG" >&2
        echo "$usage" >&2
@@ -46,13 +50,15 @@ fi
 
 echo "Running setup for ${environment} environment"
 
-read -r -p "Do you want to continue? [y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
-then
-    echo "Continuing..."
-else
-   echo "Ending execution"
-   exit
+if ${ask}; then
+    read -r -p "Do you want to continue? [y/N] " response
+    if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+    then
+        echo "Continuing..."
+    else
+       echo "Ending execution"
+       exit
+    fi
 fi
 
 
