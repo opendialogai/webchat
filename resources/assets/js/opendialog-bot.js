d2a5e740affff6084ce7f37066c03c13758b5a12
Stuart Haigh <stuarth@greenshootlabs.com>
2019-08-14 16:08:47 +0100

    adds polyfill for startWith and endsWith for IE

--- resources/assets/js/opendialog-bot.js
+++ resources/assets/js/opendialog-bot.js
@@ -6,6 +6,26 @@ import 'core-js/es7/object';
 let query = 'open=true';
 let url = '';
 
+// startsWith polyfill
+if (!String.prototype.startsWith) {
+  Object.defineProperty(String.prototype, 'startsWith', {
+    value: function(search, pos) {
+      pos = !pos || pos < 0 ? 0 : +pos;
+      return this.substring(pos, pos + search.length) === search;
+    }
+  });
+}
+
+// endsWith polyfill
+if (!String.prototype.endsWith) {
+  String.prototype.endsWith = function(search, this_len) {
+    if (this_len === undefined || this_len > this.length) {
+      this_len = this.length;
+    }
+    return this.substring(this_len - search.length, this_len) === search;
+  };
+}
+
 function addCssToPage(href) {
   const link = document.createElement('link');
   link.setAttribute('rel', 'stylesheet');
