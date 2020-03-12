ab47dd1f7e11a137b06844f1be0e237d1d3ab973
andrew <andrew.dickens@manifesto.co.uk>
2020-03-12 09:41:37 +0000

    update close button on WebChat.vue

--- resources/assets/js/components/WebChat.vue
+++ resources/assets/js/components/WebChat.vue
@@ -63,7 +63,11 @@
         @setChatMode="setChatMode"
       />
       <div class="close-chat">
-        <div class="close-chat__button" @click="toggleChatOpen"  >
+        <div
+          class="close-chat__button"
+          :class="{closeChatButtonAnimate: isOpen}"
+          @click="toggleChatOpen"
+        >
           <img src="/images/close-btn.svg" class="close-chat__img" />
         </div>
       </div>
@@ -74,8 +78,8 @@
 
 
 <script>
-  import axios from "axios";
-  import chatService from "../services/ChatService"
+import axios from "axios";
+import chatService from "../services/ChatService";
 
 const moment = require("moment-timezone");
 
@@ -242,9 +246,9 @@ export default {
       }
     },
     modeData(newValue, oldValue) {
-      if (oldValue.mode === 'custom') {
+      if (oldValue.mode === "custom") {
         this.destroyCustomMode();
-      } else if (oldValue.mode === 'webchat') {
+      } else if (oldValue.mode === "webchat") {
         this.destroyWebchatMode();
       }
 
@@ -252,11 +256,15 @@ export default {
 
       if (oldValue.mode === "custom" && newValue.mode === "webchat") {
         // Convert the Hand-to-Human message to a text message
-        let filteredMessageList = this.messageList.filter((message) => message.mode === "webchat" && message.type === 'hand-to-human');
-        let handToHumanMessage = filteredMessageList[filteredMessageList.length-1];
+        let filteredMessageList = this.messageList.filter(
+          message =>
+            message.mode === "webchat" && message.type === "hand-to-human"
+        );
+        let handToHumanMessage =
+          filteredMessageList[filteredMessageList.length - 1];
 
         if (handToHumanMessage) {
-          handToHumanMessage.type = 'text';
+          handToHumanMessage.type = "text";
           handToHumanMessage.data.text = handToHumanMessage.data.elements.text;
         }
 
@@ -268,9 +276,9 @@ export default {
         });
       }
 
-      if (newValue.mode === 'custom') {
+      if (newValue.mode === "custom") {
         this.setupCustomMode();
-      } else if (newValue.mode === 'webchat') {
+      } else if (newValue.mode === "webchat") {
         this.setupWebchatMode();
       }
     }
@@ -403,8 +411,9 @@ export default {
       }
 
       chatService.sendRequest(newMsg, this).then(
-          response => chatService.sendResponseSuccess(response, newMsg, this),
-          () => chatService.sendResponseError(null, newMsg, this));
+        response => chatService.sendResponseSuccess(response, newMsg, this),
+        () => chatService.sendResponseError(null, newMsg, this)
+      );
     },
     userInputFocus() {
       if (!this.isExpand && !this.isMobile) {
@@ -443,8 +452,8 @@ export default {
       this.onFormButtonClick(data, msg);
     },
     onFullPageFormInputCancel() {
-        const msg = this.messageList[this.messageList.length - 1];
-        this.onFormCancelClick(msg);
+      const msg = this.messageList[this.messageList.length - 1];
+      this.onFormCancelClick(msg);
     },
     onFullPageRichInputSubmit(button) {
       const msg = this.messageList[this.messageList.length - 1];
@@ -550,14 +559,14 @@ export default {
       });
     },
     onFormCancelClick(msg) {
-        console.log(msg);
-        this.messageList[this.messageList.indexOf(msg)].type = "text";
-        this.sendMessage({
-            type: "form_response",
-            author: "me",
-            callback_id: msg.data.cancel_callback,
-            data:{text: msg.data.cancel_text}
-        });
+      console.log(msg);
+      this.messageList[this.messageList.indexOf(msg)].type = "text";
+      this.sendMessage({
+        type: "form_response",
+        author: "me",
+        callback_id: msg.data.cancel_callback,
+        data: { text: msg.data.cancel_text }
+      });
     },
     onRestartButtonClick() {
       this.sendMessage({
@@ -605,7 +614,7 @@ export default {
           type: "chat_open",
           callback_id: callback,
           data: {
-            value: this.parentUrl,
+            value: this.parentUrl
           }
         };
 
@@ -651,8 +660,8 @@ export default {
 
             // Convert to the right message type for display
             if (currentMessage.type === "hand-to-human") {
-                currentMessage.data.text = currentMessage.data.elements.text;
-                currentMessage.type = "text";
+              currentMessage.data.text = currentMessage.data.elements.text;
+              currentMessage.type = "text";
             }
 
             if (
@@ -809,7 +818,7 @@ export default {
       }
     },
     setChatMode(data) {
-      this.$emit('setChatMode', data);
+      this.$emit("setChatMode", data);
     },
     destroyCustomMode() {
       chatService.destroyChat(this);
@@ -830,8 +839,9 @@ export default {
       await chatService.initialiseChat(this);
     },
     userTyping(text) {
-      chatService.sendTypingRequest(text, this)
-        .then((response) => chatService.sendTypingResponseSuccess(response, this))
+      chatService
+        .sendTypingRequest(text, this)
+        .then(response => chatService.sendTypingResponseSuccess(response, this))
         .catch(() => chatService.sendTypingResponseError(null, this));
     }
   }
