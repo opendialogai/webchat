66a1374725ce8525e50a924afc046583c9b16f96
Nicolò Caruso <nicolo@istos.it>
2020-03-18 12:15:25 +0100

    Remove CTA text on chat open

--- resources/assets/js/components/WebChat.vue
+++ resources/assets/js/components/WebChat.vue
@@ -594,6 +594,8 @@ export default {
           this.$emit("toggleChatOpen", this.headerHeight);
         }, 300);
       } else {
+        this.ctaText = [];
+
         this.isOpen = !this.isOpen;
         this.$emit("toggleChatOpen", this.headerHeight);
           window.parent.postMessage(
