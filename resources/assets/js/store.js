ebf098d906889d55f6bc58dd4a43fe9f29f1554b
elliotmassen <elliotm@greenshootlabs.com>
2020-04-20 15:28:28 +0100

    Collecting all relevant contextual information from within opendialog-bot.js and POST-ing to backend

--- resources/assets/js/store.js
+++ resources/assets/js/store.js
@@ -10,6 +10,7 @@ const store = new Vuex.Store({
   state: {
     apiReady: false,
     uuid: sessionStorage.uuid || null,
+    settings: sessionStorage.openDialogSettings ? JSON.parse(sessionStorage.openDialogSettings) : null
   },
   mutations: {
     setApiReady(state, val) {
