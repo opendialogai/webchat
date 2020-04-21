458a95176bf1919f983d1142a818d3bc96f5c1b2
elliotmassen <elliotm@greenshootlabs.com>
2020-04-21 10:25:39 +0100

    Adding default webchat settings file

---
+++ resources/assets/js/default-webchat-settings.js
@@ -0,0 +1,33 @@
+export default {
+    "url": "",
+    "mobileWidth": "1",
+    "general": {
+        "url": "/web-chat",
+        "teamName": "Webchat",
+        "chatbotCssPath": "",
+        "chatbotFullpageCssPath": "",
+        "chatbotName": "Bot",
+        "chatbotAvatarPath": "/images/avatar.svg",
+        "restartButtonCallback": "WELCOME",
+        "open": true,
+        "hideDatetimeMessage": true,
+        "disableCloseChat": false,
+        "useBotAvatar": true,
+        "useHumanAvatar": false,
+        "useBotName": false,
+        "useHumanName": false,
+        "collectUserIp": false,
+        "showRestartButton": false,
+        "messageAnimation": false,
+        "hideTypingIndicatorOnInternalMessages": false,
+        "messageDelay": 500,
+        "logo": "/images/logo.svg",
+    },
+    "comments": {
+        "commentsEnabled": false,
+    },
+    "webchatHistory": {
+        "showHistory": true,
+        "numberOfMessages": 10
+    }
+};
