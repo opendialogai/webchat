d41b05b2c8ab22f7ccf582d8f82af86c18deb0a2
elliotmassen <elliotm@greenshootlabs.com>
2020-08-27 12:07:56 +0100

    Removing reference to custom chat mode from MessageList

--- resources/assets/js/components/MessageList.vue
+++ resources/assets/js/components/MessageList.vue
@@ -123,7 +123,7 @@ export default {
     shouldShowMessage(message) {
       let isModeSame = message.mode === this.modeData.mode;
       let isWebchatMode = message.mode === 'webchat';
-      let isCustomMode = message.mode === 'custom';
+      let isCustomMode = !isWebchatMode;
       let isFromSameInstance = message.modeInstance === this.modeData.modeInstance;
       return (isModeSame && isWebchatMode) || (isModeSame && isCustomMode && isFromSameInstance);
     },
@@ -153,7 +153,7 @@ export default {
   @media (min-width: $media-med) {
     padding: 40px 0 0;
   }
-    
+
   &.fadeUp-enter-active {
     animation-duration: 0.5s;
   }
