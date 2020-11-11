47d0a55c2e1e2e122876f645a45d911bd4b347f4
lukatabinns <lukatab@greenshootlabs.com>
2020-11-11 19:08:38 +0000

    admin to be able to give permission to user access to webchat url

--- tests/Feature/AllowAccessToWebChatUrlTest.php
+++ tests/Feature/AllowAccessToWebChatUrlTest.php
@@ -8,50 +8,65 @@ use OpenDialogAi\Core\Tests\TestCase;
 use OpenDialogAi\Webchat\Http\Middleware\WebChatMiddleware;
 use \OpenDialogAi\Webchat\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use OpenDialogAi\Webchat\WebchatSetting;
 
 class AllowAccessToWebChatUrlTest extends TestCase
 {
     use RefreshDatabase;
 
     public function testSettingsWebChatPermissionUserLoggedIn() {
-        $user = new User();
-        $user->name = 'jamie';
-        $user->email = 'jamie@example.com';
-        $user->password  = bcrypt('secret');
-        $user->save();
-
-         Auth::shouldReceive('check')->once()->andReturn(true);
-         Auth::shouldReceive('user')->once()->andReturn($user);
-
-         $this->actingAs($user);
-
-         $request = Request::create('/web-chat', 'GET');
-
-         $middleware = new WebChatMiddleware();
-
-         $response = $middleware->handle($request, function () {});
-
-         $this->assertEquals($response->getStatusCode(), null);
+        $this->webChatSetting(FALSE);
+        $this->userCreate();
+        $this->runMiddleWare(null);
      }
 
      public function testSettingsWebChatPermissionUserNotLoggedIn() {
-         $user = new User();
-         $user->name = 'billy';
-         $user->email = 'billy@example.com';
-         $user->password  = bcrypt('secret');
-         $user->save();
-
-         Auth::shouldReceive('check')->once()->andReturn(false);
-         Auth::shouldReceive('user')->once()->andReturn($user);
-
-         $this->actingAs($user);
-
-         $request = Request::create('/web-chat', 'GET');
-
-         $middleware = new WebChatMiddleware();
-
-         $response = $middleware->handle($request, function () {});
-
-         $this->assertEquals($response->getStatusCode(), 302);
+         $this->webChatSetting(FALSE);
+         $this->userCreate();
+         $this->runMiddleWare(302);
      }
+
+    public function testSettingsWebChatPermissionUserIsTrueAndNotLoggedIn() {
+        $this->webChatSetting(TRUE);
+        $this->userCreate();
+        $this->runMiddleWare(null);
+    }
+
+    public function testSettingsWebChatPermissionUserIsTrueAndLoggenIn() {
+        $this->webChatSetting(TRUE);
+        $this->userCreate();
+        $this->runMiddleWare(null);
+
+    }
+
+    public function webChatSetting($val){
+        $setting = new WebchatSetting();
+        $setting->name = $setting::WEBCHAT_FULL_PAGE_PUBLIC;
+        $setting->value = $val;
+        $setting->type = 'boolean';
+        $setting->save();
+    }
+
+    public function userCreate(){
+        $user = new User();
+        $user->name = 'billy';
+        $user->email = 'billy@example.com';
+        $user->password  = bcrypt('secret');
+        $user->save();
+
+        Auth::shouldReceive('check')->once()->andReturn(true);
+        Auth::shouldReceive('user')->once()->andReturn($user);
+
+        $this->actingAs($user);
+    }
+
+    public function runMiddleWare($result){
+        $request = Request::create('/web-chat', 'GET');
+
+        $middleware = new WebChatMiddleware();
+
+        $response = $middleware->handle($request, function () {});
+
+        $this->assertEquals($response->getStatusCode(), $result);
+    }
 }
