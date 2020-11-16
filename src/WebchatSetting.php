8827546a7be217080136c951ed31cdffce6cf86f
lukatabinns <lukatab@greenshootlabs.com>
2020-11-16 17:44:46 +0000

    Administrator to be able to give permission to user access to webchat url.

--- src/WebchatSetting.php
+++ src/WebchatSetting.php
@@ -156,8 +156,7 @@ class WebchatSetting extends Model
     {
         $val = WebchatSetting::getWebChatSetting(WebchatSetting::WEBCHAT_FULL_PAGE_PUBLIC);
 
-        if ($val != null)
-        {
+        if ($val != null) {
             return (bool)$val->value;
         }
     }
