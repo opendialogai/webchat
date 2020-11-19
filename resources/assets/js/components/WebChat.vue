df408a40381c289b4d140016db44dccee083b3c7
elliotmassen <elliotm@greenshootlabs.com>
2020-11-19 15:30:18 +0000

    Fixing form response setting bug

--- resources/assets/js/components/WebChat.vue
+++ resources/assets/js/components/WebChat.vue
@@ -577,48 +577,48 @@ export default {
 
       const responseData = {};
 
+      const newMessageText = [];
+
+      msg.data.elements.forEach(element => {
+        let val
+
+        if (element.element_type === 'checkbox') {
+          let attribute = [];
+          let text = [];
+          Object.keys(element.options)
+              .forEach(option => {
+                if (data[element.name][option] === true) {
+                  attribute.push(option)
+                  text.push(element.options[option])
+                }
+              });
+
+          responseData[element.name] = attribute.join(",");
+          val = text.join(", ");
+        } else {
+          responseData[element.name] = data[element.name].value;
+
+          if (element.element_type === 'select' && data[element.name].value !== '') {
+            val = element.options[data[element.name].value]
+          } else if (element.element_type === 'auto-select' && data[element.name].value !== '') {
+            val = element.options.find(obj => obj.key === data[element.name].value).value
+          } else {
+            val = data[element.name].value
+          }
+        }
+
+        if (element.display) {
+          newMessageText.push(
+              `${element.display}: ${val}`
+          );
+        } else {
+          newMessageText.push(val);
+        }
+      });
+
       if (this.$store.state.settings.general.formResponseText) {
         responseData.text = this.$store.state.settings.general.formResponseText;
       } else {
-        const newMessageText = [];
-
-        msg.data.elements.forEach(element => {
-          let val
-
-          if (element.element_type === 'checkbox') {
-            let attribute = [];
-            let text = [];
-            Object.keys(element.options)
-                .forEach(option => {
-                  if (data[element.name][option] === true) {
-                    attribute.push(option)
-                    text.push(element.options[option])
-                  }
-                });
-
-            responseData[element.name] = attribute.join(",");
-            val = text.join(", ");
-          } else {
-            responseData[element.name] = data[element.name].value;
-
-            if (element.element_type === 'select' && data[element.name].value !== '') {
-              val = element.options[data[element.name].value]
-            } else if (element.element_type === 'auto-select' && data[element.name].value !== '') {
-              val = element.options.find(obj => obj.key === data[element.name].value).value
-            } else {
-              val = data[element.name].value
-            }
-          }
-
-          if (element.display) {
-            newMessageText.push(
-                `${element.display}: ${val}`
-            );
-          } else {
-            newMessageText.push(val);
-          }
-        });
-
         responseData.text = newMessageText.join("\n");
       }
 
