5eb0f802987297c169a06fb837334795ba74c84e
elliotmassen <elliotm@greenshootlabs.com>
2020-11-20 10:22:06 +0000

    Fixing broken test

--- src/WebchatSetting/Service/WebchatSettingService.php
+++ src/WebchatSetting/Service/WebchatSettingService.php
@@ -13,4 +13,34 @@ class WebchatSettingService implements WebchatSettingServiceInterface
             ));
         }
     }
+
+    /**
+     * From: https://gist.github.com/koenhoeijmakers/0a8e326ee3b12a826d73be38693fb647
+     *
+     * @param array $original
+     * @param array $merging
+     * @return array
+     */
+    private function recursiveMergeConfigFrom(array $original, array $merging)
+    {
+        $array = array_merge($original, $merging);
+
+        foreach ($original as $key => $value) {
+            if (!is_array($value)) {
+                continue;
+            }
+
+            if (!Arr::exists($merging, $key)) {
+                continue;
+            }
+
+            if (is_numeric($key)) {
+                continue;
+            }
+
+            $array[$key] = $this->recursiveMergeConfigFrom($value, $merging[$key]);
+        }
+
+        return $array;
+    }
 }
--- src/WebchatSetting/WebchatSettingServiceProvider.php
+++ src/WebchatSetting/WebchatSettingServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace OpenDialogAi\Webchat\WebchatSetting;
 
-use Illuminate\Support\Arr;
 use Illuminate\Support\ServiceProvider;
 use OpenDialogAi\Webchat\WebchatSetting\Service\WebchatSettingService;
 use OpenDialogAi\Webchat\WebchatSetting\Service\WebchatSettingServiceInterface;
@@ -28,35 +27,5 @@ class WebchatSettingServiceProvider extends ServiceProvider
     public function mergeConfigFrom($path, $key)
     {
         resolve(WebchatSettingServiceInterface::class)->mergeConfigFrom($path, $key);
-    }
-
-    /**
-     * From: https://gist.github.com/koenhoeijmakers/0a8e326ee3b12a826d73be38693fb647
-     *
-     * @param array $original
-     * @param array $merging
-     * @return array
-     */
-    private function recursiveMergeConfigFrom(array $original, array $merging)
-    {
-        $array = array_merge($original, $merging);
-
-        foreach ($original as $key => $value) {
-            if (!is_array($value)) {
-                continue;
-            }
-
-            if (!Arr::exists($merging, $key)) {
-                continue;
-            }
-
-            if (is_numeric($key)) {
-                continue;
-            }
-
-            $array[$key] = $this->recursiveMergeConfigFrom($value, $merging[$key]);
-        }
-
-        return $array;
     }
 }
