47bae892c2e28fcb9a0292cee97decb00a698340
Stuart Haigh <stuarth@greenshootlabs.com>
2021-06-15 17:17:02 +0100

    uses the correct file for the migration publishing config

--- config/webchat.php
+++ config/webchat.php
@@ -4,5 +4,9 @@
  * Config for the webchat component
  */
 return [
-    'allow-fullpage' => env('ALLOW_FULLPAGE_WEBCHAT', false)
+    'allow-fullpage' => env('ALLOW_FULLPAGE_WEBCHAT', false),
+
+    'multi_tenant' => env('MULTI_TENANT', false),
+
+    'migration_publish_dir' => env('MULTI_TENANT', false) ? 'migrations/tenant' : 'migrations'
 ];
